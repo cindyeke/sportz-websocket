c131e110b095ccf6605a92257d40cbac25c2b604
Cindy Eke <officialcee1@gmail.com>
2026-07-20 19:27:27 +0100

    implement database schemas

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hey");
 });
 
